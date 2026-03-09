56027b934801eb61453529a61443a51d9bd58086
Brian0213 <91477623+Brian0213@users.noreply.github.com>
2026-03-09 19:04:56 -0400

    Update conftest.py

--- testScripts/conftest.py
+++ testScripts/conftest.py
@@ -95,7 +95,7 @@ def setup(browser):
     options.add_argument("--disable-popup-blocking")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
-    options.add_argument("--headless=new"")
+    options.add_argument("--headless=new")
     options.add_argument("--disable-dev-shm-usage")
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
